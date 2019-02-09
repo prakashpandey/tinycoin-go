c0dbd3c19ba25a042a3970d8eba5c18e360e2c8e
Prakash Pandey <prakashpandeyy@yahoo.com>
2019-02-09 17:49:24 +0530

    keep on generating blocks infinitely

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/prakashpandey/gochain/block"
+	"time"
 )
 
+// Initialize a new blockchain by creating a
+// genesis block
 var genesisBlock = block.GenesisBlock()
 var blockchain = block.NewBlockchain(genesisBlock)
+var prevBlock = genesisBlock
+
+func generateNewBlock() {
+	newBlock := block.NewBlock(prevBlock)
+	blockchain.Add(newBlock)
+	prevBlock = newBlock
+}
 
 func main() {
-	newBlock := block.NewBlock(genesisBlock)
-	blockchain.Add(newBlock)
-	fmt.Printf("%s\n", blockchain.String())
+	for i := 0; i < 100000; i++ {
+		generateNewBlock()
+		fmt.Printf("%s\n", blockchain.String())
+		time.Sleep(1 * time.Second)
+	}
 }
